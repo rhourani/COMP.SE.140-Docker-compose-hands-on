73c3090b0d87ac2de8933f11f11daa607f171262
Ridvan <dadrad30@gmail.com>
2025-01-20 23:25:38 +0200

    Develop full scenario in the tests, commented shutdown test as it shutdown the containers

--- Service1/tests/service1.test.js
+++ Service1/tests/service1.test.js
@@ -1,21 +1,37 @@
 const request = require('supertest'); 
-const app ='service1:8199';
-
+const app ='localhost:8197';
 
 const authHeader = 'Basic ' + Buffer.from('ridvan:ridvan').toString('base64');
 
 describe('API Tests', () => {
   describe('PUT /state', () => { 
-    it('should set state to INIT', async () => { 
+    it('should set state to RUNNING when user first log in', async () => { 
       const res = await request(app) 
       .put('/state')
       .set('Authorization', authHeader)
       .set('Content-Type', 'text/plain') 
       .send('INIT'); 
       expect(res.status).toBe(200); 
-      expect(res.text).toBe('Logged out'); 
+      expect(res.text).toBe('RUNNING'); 
     }); 
     
+    it('should set state to RUNNING', async () => { 
+      //Reset system
+      await request(app) 
+      .put('/state')
+      .set('Authorization', authHeader)
+      .set('Content-Type', 'text/plain') 
+      .send('INIT'); 
+
+      const res = await request(app) 
+      .put('/state')
+      .set('Authorization', authHeader)
+      .set('Content-Type', 'text/plain') 
+      .send('RUNNING'); 
+      expect(res.status).toBe(200); 
+      expect(res.text).toBe('RUNNING'); 
+    }); 
+
     it('should set state to PAUSED', async () => { 
       const res = await request(app) 
       .put('/state')
@@ -25,27 +41,7 @@ describe('API Tests', () => {
       expect(res.status).toBe(200); 
       expect(res.text).toBe('PAUSED'); 
     }); 
-    
-    it('should set state to RUNNING', async () => { 
-      const res = await request(app) 
-      .put('/state')
-      .set('Authorization', authHeader)
-      .set('Content-Type', 'text/plain') 
-      .send('RUNNING'); 
-      expect(res.status).toBe(200); 
-      expect(res.text).toBe('RUNNING'); 
-    }); 
-    
-    it('should set state to SHUTDOWN', async () => { 
-      const res = await request(app) 
-      .put('/state')
-      .set('Authorization', authHeader)
-      .set('Content-Type', 'text/plain') 
-      .send('SHUTDOWN'); 
-      expect(res.status).toBe(200); 
-      expect(res.text).toBe('Shutting down performed'); 
-    }); 
-    
+
     it('should do nothing if state is the same as previous', async () => { 
       await request(app) 
       .put('/state')
@@ -59,8 +55,18 @@ describe('API Tests', () => {
       .set('Content-Type', 'text/plain') 
       .send('RUNNING'); 
       expect(res.status).toBe(200); 
-      expect(res.text).toBe('RUNNING'); 
+      expect(res.text).toBe('No change done'); 
     }); 
+    
+    // it('should set state to SHUTDOWN', async () => { 
+    //   const res = await request(app) 
+    //   .put('/state')
+    //   .set('Authorization', authHeader)
+    //   .set('Content-Type', 'text/plain') 
+    //   .send('SHUTDOWN'); 
+    //   expect(res.status).toBe(200); 
+    //   expect(res.text).toBe('Shutting down performed'); 
+    // });
   });
 
 
